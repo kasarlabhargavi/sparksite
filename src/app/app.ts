f9dd6be25ba7b0fdf8ecfe875fd6e0397c09a81d
pranaykumarsamala <pranaykumarsamala1614@gmail.com>
2025-06-05 11:43:14 +0530

    installed plugins

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import Aos from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: false,
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit{
   protected title = 'spark';
+  isCollapsed = true;
+
+  ngOnInit(): void {
+    Aos.init();
+  }
+
 }
